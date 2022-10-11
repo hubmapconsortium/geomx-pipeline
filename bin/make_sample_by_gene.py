403b155564847432f3a0e346037f28282b444d8c
Sean Donahue <seandonahue5311@gmail.com>
2022-10-11 10:15:40 -0400

    Adding mapping from gene symbol to ensembl_ids (retaining gene symbols as column in adata.var)

--- bin/make_sample_by_gene.py
+++ bin/make_sample_by_gene.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Dict
 from argparse import ArgumentParser
 import os
 import anndata
 import json
+import pandas as pd
+
+GENE_MAPPING_DIRECTORIES = [
+    Path(__file__).parent.parent / 'data',
+    Path('/opt/data'),
+]
 
 def find_files(directory: Path, pattern: str) -> Iterable[Path]:
     for dirpath_str, dirnames, filenames in os.walk(directory):
@@ -14,6 +20,40 @@ def find_files(directory: Path, pattern: str) -> Iterable[Path]:
             filepath = dirpath / filename
             if filepath.match(pattern):
                 yield filepath
+
+
+def read_gene_mapping(reverse=False) -> Dict[str, str]:
+    """
+    Try to find the Ensembl to HUGO symbol mapping, with paths suitable
+    for running this script inside and outside a Docker container.
+    :return:
+    """
+
+    filename = 'ensembl_to_symbol.json' if reverse else 'symbol_to_ensembl.json'
+
+    for directory in GENE_MAPPING_DIRECTORIES:
+        mapping_file = directory / filename
+        if mapping_file.is_file():
+            with open(mapping_file) as f:
+                return json.load(f)
+    message_pieces = ["Couldn't find Ensembl → HUGO mapping file. Tried:"]
+    message_pieces.extend(f'\t{path}' for path in GENE_MAPPING_DIRECTORIES)
+    raise ValueError('\n'.join(message_pieces))
+
+def map_gene_ids(adata):
+    obsm = adata.obsm
+    gene_mapping = read_gene_mapping()
+    keep_vars = [gene in gene_mapping for gene in adata.var.index]
+    adata = adata[:, keep_vars]
+    temp_df = pd.DataFrame(adata.X.todense(), index=adata.obs.index, columns=adata.var.index)
+    aggregated = temp_df.groupby(level=0, axis=1).sum()
+    adata = anndata.AnnData(aggregated, obs=adata.obs)
+    adata.var.index = [gene_mapping[var] for var in adata.var.index]
+    reverse_dict = read_gene_mapping(reverse=True)
+    adata.var["gene_symbol"] = pd.Series([reverse_dict[ensembl_id] for ensembl_id in adata.var.index])
+    adata.obsm = obsm
+    adata.var_names_make_unique()
+    return adata
 
 def get_probe_to_gene_symbol_mapping(pkc_file):
     with open(pkc_file) as f:
@@ -46,6 +86,8 @@ def main(data_directory: Path):
         for probe_id in code_summaries[aoi_id]:
             gene_symbol = probe_to_gene_mapping[probe_id]
             adata[aoi_id, gene_symbol] = code_summaries[aoi_id][probe_id]
+
+    adata = map_gene_ids(adata)
     adata.write_h5ad('cell_by_gene.h5ad')
 
 if __name__ == '__main__':
