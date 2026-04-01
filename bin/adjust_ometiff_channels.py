cd62f46a33e5855d547ba8a2573a612c6e670489
Matt Ruffalo <matt.ruffalo@gmail.com>
2026-04-01 14:26:29 -0400

    Overhaul OME-XML adjustment
    Rather than reading all pixel data and writing a new OME-TIFF from scratch,
    tweak the OME-XML as little as possible, and replace it with tiffcomment.

--- bin/adjust_ometiff_channels.py
+++ bin/adjust_ometiff_channels.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+import shlex
 from argparse import ArgumentParser
+from os import fspath
 from pathlib import Path
 from shutil import copy
+from subprocess import run
 
-import bioio
 import pandas as pd
-from bioio.writers import OmeTiffWriter
-from bioio_base.dimensions import DimensionNames
+import tifffile
+from lxml import etree
 
 
 def find_ome_tiff(data_dir: Path) -> Path:
@@ -22,23 +24,42 @@ def main(data_dir: Path):
     print("Found OME-TIFF:", ome_tiff)
     output_ometiff_path = Path(ome_tiff.name)
     maybe_markers_csv = data_dir / "raw/markers.csv"
+    print("Copying", ome_tiff, "to", output_ometiff_path)
+    copy(ome_tiff, output_ometiff_path)
     if maybe_markers_csv.is_file():
         print("Found markers CSV:", maybe_markers_csv)
         markers = pd.read_csv(maybe_markers_csv)
-        image = bioio.BioImage(ome_tiff)
-        print("Old channel names:", image.channel_names)
+        image = tifffile.TiffFile(ome_tiff)
+        tree = etree.fromstring(image.ome_metadata)
+        namespaces = tree.nsmap.copy()
+        namespaces["OME"] = namespaces[None]
+        del namespaces[None]
+        channel_nodes = tree.xpath("//OME:Pixels/OME:Channel", namespaces=namespaces)
+        print("Old channel names:", [c.attrib["Name"] for c in channel_nodes])
         # handle CSVs with junk rows at bottom
-        new_markers = list(markers["marker"][: len(image.channel_names)])
-        print("Instantiating new image with channel names:", new_markers)
-        new_image = bioio.BioImage(
-            image.xarray_dask_data.assign_coords({DimensionNames.Channel: new_markers}),
-            physical_pixel_sizes=image.physical_pixel_sizes,
-        )
-        print("Saving image to", output_ometiff_path)
-        new_image.save(output_ometiff_path)
+        new_markers = list(markers["marker"][: len(channel_nodes)])
+        print("Adjusting OME-XML with new channel names:", new_markers)
+        for channel_node, new_marker in zip(channel_nodes, new_markers):
+            # TODO: figure out a better attribute name
+            channel_node.attrib["OrigName"] = channel_node.attrib["Name"]
+            channel_node.attrib["Name"] = new_marker
+        new_ome_xml = etree.tostring(tree, encoding="utf-8")
+        with open("temp.xml", "wb") as f:
+            f.write(new_ome_xml)
+        # Hack: adjust image file as little as possible; the Python tifffile library
+        # could adjust image plane order, and the data could get out of sync with
+        # the original OME-XML metadata (which we also modified as little as possible).
+        # So, call out to `tiffcomment` to do this adjustment with the new OME-XML.
+        command = [
+            "tiffcomment",
+            "--set-file",
+            "temp.xml",
+            fspath(output_ometiff_path),
+        ]
+        print("Running:", shlex.join(command))
+        run(command)
     else:
-        print("No markers CSV; copying OME-TIFF to", output_ometiff_path)
-        copy(ome_tiff, output_ometiff_path)
+        print("No markers CSV; leaving OME-TIFF unchanged")
 
 
 if __name__ == "__main__":
